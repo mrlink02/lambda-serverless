0fe894774d5430cac9133e592970101b403a521b
Jhon.Mora <jfmora.9302@gmail.com>
2022-09-21 00:07:14 +0000

    inicial

--- dynamo/dynamo.sh
+++ dynamo/dynamo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-east-1 \
---parameter-overrides DynamoName="lablambda" DynamoKey="dni" \
---stack-name dynamo-lambda-lab \
+--parameter-overrides DynamoName="jmlablambda" DynamoKey="CC" \
+--stack-name jhonmora-dynamo-lambda-lab \
 --template-file ./dynamo.yml
--- prereq/aws-codepipeline-prereq.sh
+++ prereq/aws-codepipeline-prereq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-east-1 \
---stack-name codepipeline-lambda-prereq \
---template-file ./aws-codepipeline-prereq.yml \
+--stack-name jhonmora-codepipeline-lambda-prereq \
+--template-file ./jmaws-codepipeline-prereq.yml \
 --capabilities CAPABILITY_IAM \
 --capabilities CAPABILITY_NAMED_IAM
